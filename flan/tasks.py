960a55eaa55f8de6c5ba67c61b6106b7848610ea
Jason Phang <jasonphang@nyu.edu>
2022-06-17 21:32:50 -0700

    flan_adapt

--- flan/tasks.py
+++ flan/tasks.py
@@ -35,7 +35,7 @@ from flan import utils
 ShotConfig = few_shot.ShotConfig
 
 # This is a placeholder, for the paper we used an internal vocabulary and model.
-VOCAB_FILE = 'gs://t5-data/vocabs/mc4.250000.100extra/sentencepiece.model'
+VOCAB_FILE = 'gs://t5-data/vocabs/cc_all.32000.100extra/sentencepiece.model'
 FLAN_VOCABULARY = seqio.SentencePieceVocabulary(VOCAB_FILE)
 
 FLAN_OUTPUT_FEATURES = {
