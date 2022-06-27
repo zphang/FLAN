2a67cf1d62249f1494587c413902296f6f7a013a
Jason Phang <jasonphang@nyu.edu>
2022-06-27 13:53:46 -0700

    minor fixes

--- flanmixture_nli_paraphrase.py
+++ flanmixture_nli_paraphrase.py
@@ -22,8 +22,10 @@ from flan import task_splits
 from flan import tasks  # pylint: disable=unused-import
 from flan import templates  # pylint: disable=unused-import
 
+mixing_rate_3k = functools.partial(seqio.mixing_rate_num_examples, maximum=3000)
+
 shot_config = task_splits.ShotConfig.ZERO
-task_clusters =  task_splits._get_default_task_clusters(10, shot_config)
+task_clusters = task_splits._get_default_task_clusters(10, shot_config)
 
 test_tasks = []
 train_tasks = []
